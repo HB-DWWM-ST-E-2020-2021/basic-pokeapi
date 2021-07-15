8caf9afafa12ba49ee8c657bb425ffcaaf34fd18
Benjamin Georgeault <benjamin@drosalys.fr>
2021-07-15 15:48:27 +0200

    Add composer.

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * This file is part of the basic-pokeapi package.
+ *
+ * (c) Benjamin Georgeault
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
