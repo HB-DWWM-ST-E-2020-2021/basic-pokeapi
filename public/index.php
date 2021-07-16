e4f059f96a34f09df5c0c40155e203fd3e7ecc72
Benjamin Georgeault <benjamin@drosalys.fr>
2021-07-16 11:01:25 +0200

    Add Router

--- public/index.php
+++ public/index.php
@@ -17,5 +17,14 @@ $pokedex = new \Hb\BasicPokeapi\Pokedex();
 // Set the Content-Type HTTP header for the response to JSON type.
 header('Content-Type: application/json');
 
-// Query data with Pokedex and convert to JSON to be send to the client.
-echo json_encode($pokedex->getAllPokemon());
+if (!isset($_SERVER['PATH_INFO']) || '/pokemon' === $_SERVER['PATH_INFO']) {
+    // Query data with Pokedex and convert to JSON to be send to the client.
+    echo json_encode($pokedex->getAllPokemon());
+} elseif ('/pikachu' === $_SERVER['PATH_INFO']) {
+    echo json_encode($pokedex->getCleanPikachu());
+} else {
+    http_response_code(404);
+    echo json_encode([
+        'error' => 'Wrong path.',
+    ]);
+}
